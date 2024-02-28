6c434f8d78261ee2a939c8e0099b7f8a8aba788c
Hareend <Hareend@Hareend>
2024-02-28 07:39:26 +0530

    Super Keyword

---
+++ Assignment/src/main/java/SuperKeyword/Constructor1.java
@@ -0,0 +1,10 @@
+package SuperKeyword;
+
+public class Constructor1 {
+	
+	public Constructor1()
+	{
+		System.out.println("Parent Class Constructor");
+	}
+
+}
---
+++ Assignment/src/main/java/SuperKeyword/Constructor2.java
@@ -0,0 +1,16 @@
+package SuperKeyword;
+
+public class Constructor2 extends Constructor1{
+	public Constructor2()
+	{
+		super();
+		System.out.println("Child Class");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Constructor2 obj = new Constructor2();
+		
+	}
+
+}
