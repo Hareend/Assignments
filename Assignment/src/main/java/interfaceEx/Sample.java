bb6c2230e84326175c169208d47854b7e87c56d8
Hareend <Hareend@Hareend>
2024-02-28 07:37:19 +0530

    Interface Examples

---
+++ Assignment/src/main/java/interfaceEx/Sample.java
@@ -0,0 +1,5 @@
+package interfaceEx;
+
+public interface Sample {
+	public void display();
+}
---
+++ Assignment/src/main/java/interfaceEx/Sample1.java
@@ -0,0 +1,16 @@
+package interfaceEx;
+
+public class Sample1 implements Sample {
+
+	@Override
+	public void display() {
+		// TODO Auto-generated method stub
+		System.out.println("This is an Interface");
+	}
+	public static void main(String args[])
+	{
+		Sample1  obj = new Sample1();
+		obj.display();
+	}
+
+}
