89090825a8863cf260634e8c0b5af32ef5a4fdf9
Hareend <Hareend@Hareend>
2024-02-28 20:03:19 +0530

    change

--- Assignment/src/main/java/interfaceEx/Sample1.java
+++ Assignment/src/main/java/interfaceEx/Sample1.java
@@ -5,7 +5,7 @@ public class Sample1 implements Sample {
 	@Override
 	public void display() {
 		// TODO Auto-generated method stub
-		System.out.println("This is an Interface");
+		System.out.println("This is an Interface...");
 	}
 	public static void main(String args[])
 	{
