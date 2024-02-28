e441486d57828d986f20dd05c83c41e3b7097098
Hareend <Hareend@Hareend>
2024-02-28 07:38:46 +0530

    Super Keyword

---
+++ Assignment/src/main/java/SuperKeyword/AdditionClass1.java
@@ -0,0 +1,10 @@
+package SuperKeyword;
+
+public class AdditionClass1 {
+	int num1,num2,sum;
+	public void addition(int num1, int num2)
+	{
+		sum = num1+num2;
+	}
+
+}
---
+++ Assignment/src/main/java/SuperKeyword/AdditionClass2.java
@@ -0,0 +1,14 @@
+package SuperKeyword;
+
+public class AdditionClass2 extends AdditionClass1{
+	public void result()
+	{
+		super.addition(num1, num2);
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
