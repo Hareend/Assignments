64e9279ceab011fa141ad6182dcdda0d20dc8b8f
Hareend <Hareend@Hareend>
2024-02-29 11:21:10 +0530

    Super keyword

--- Assignment/src/main/java/SuperKeyword/AdditionClass1.java
+++ Assignment/src/main/java/SuperKeyword/AdditionClass1.java
@@ -2,9 +2,12 @@ package SuperKeyword;
 
 public class AdditionClass1 {
 	int num1,num2,sum;
-	public void addition(int num1, int num2)
+	public void addition()
 	{
+		num1 = 10;
+		num2 = 10;
 		sum = num1+num2;
+		System.out.println("Sum of two numbers is: "+sum);
 	}
 
 }
--- Assignment/src/main/java/SuperKeyword/AdditionClass2.java
+++ Assignment/src/main/java/SuperKeyword/AdditionClass2.java
@@ -3,12 +3,22 @@ package SuperKeyword;
 public class AdditionClass2 extends AdditionClass1{
 	public void result()
 	{
-		super.addition(num1, num2);
+		super.addition();
+		if(sum%10==0)
+		{
+			System.out.println("Number is Divisible by 10");
+		}
+		else
+		{
+			System.out.println("Number is not Divisible by 10");
+		}
 	}
+	
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		AdditionClass2 obj = new AdditionClass2();
+		obj.result();
 	}
 
 }
