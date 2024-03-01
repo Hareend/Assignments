15de7a6a863ca365d168f501f85b51aeaa89ed08
Hareend <Hareend@Hareend>
2024-03-01 08:16:15 +0530

    Exception

---
+++ Assignment/src/main/java/exception/IndexOutException.java
@@ -0,0 +1,13 @@
+package exception;
+
+public class IndexOutException {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int ar[] = {1,2,3,4,5,6};
+		for(int i=0; i<=ar.length;i++)
+			System.out.println(ar[i]);
+
+	}
+
+}
