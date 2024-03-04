12b5c600995493538756959b5f681533bd270a1f
Hareend <Hareend@Hareend>
2024-03-04 21:00:39 +0530

    List

---
+++ Assignment/src/main/java/list/List1.java
@@ -0,0 +1,19 @@
+package list;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class List1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List<String> obj = new ArrayList<>();
+		{
+			obj.add("Hello");
+			obj.add("Good");
+			obj.add("Morning");
+		}
+		System.out.println(obj);
+	}
+
+}
