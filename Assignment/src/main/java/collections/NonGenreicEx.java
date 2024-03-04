d05c3284599e4d8849600c26e2f927433ce57078
Hareend <Hareend@Hareend>
2024-03-04 20:51:06 +0530

    Non Generic Collections

---
+++ Assignment/src/main/java/collections/NonGenreicEx.java
@@ -0,0 +1,17 @@
+package collections;
+
+import java.util.ArrayList;
+
+public class NonGenreicEx {
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList obj = new ArrayList();
+		obj.add(1);
+		obj.add("Name");
+		obj.add('a');
+		System.out.println(obj);
+	}
+
+}
