1cdaef54d6bf47b5846c4f0d1067e32fe1008263
Hareend <Hareend@Hareend>
2024-03-08 19:48:16 +0530

    iterator

---
+++ Assignment/src/main/java/iterator/Remove.java
@@ -0,0 +1,27 @@
+package iterator;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class Remove {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<String>colors = new ArrayList<String>();
+		colors.add("Red");
+		colors.add("Black");
+		colors.add("Yellow");
+		
+		Iterator<String> it = colors.iterator();
+		while(it.hasNext())
+		{
+			System.out.println(it.next());
+			//it.next();
+			
+		}
+		it.remove();
+		System.out.println(colors);
+		
+	}
+
+}
